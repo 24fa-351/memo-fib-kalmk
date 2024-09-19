9307898f3fa19ce5300984ed9f623dfeecc92954
kalmk <111930167+kalmk@users.noreply.github.com>
2024-09-19 10:00:10 -0700

    Update fib.c

--- fib.c
+++ fib.c
@@ -18,21 +18,25 @@ int main(int argc, char *argv[])
     
     int N = first_arg;
     
-    if (second_arg == 'i') 
+    if (second_arg == 'i') {
         printf("%llu\n", fib_i(N));
-    else if (second_arg == 'r')
+    } else if (second_arg == 'r') {
         printf("%llu\n", fib_r(N));
+
+    }
 
     return 0;
 }
 
 unsigned long long fib_i_core(int index)
 {
-    if (index == 1)
+    if (index == 1) {
         return 0;
+    }
 
-    if (index == 2)
+    if (index == 2) {
         return 1;
+    }
 
     --index;
 
@@ -40,7 +44,7 @@ unsigned long long fib_i_core(int index)
     unsigned long long t2 = 1;
     unsigned long long next_term = t1 + t2;
 
-    for (int i = 3; i <= index; ++i)
+    for (int ix = 3; ix <= index; ++ix)
     {
         t1 = t2;
         t2 = next_term;
@@ -52,11 +56,13 @@ unsigned long long fib_i_core(int index)
 
 unsigned long long fib_r_core(int index)
 {
-    if (index == 1)
+    if (index == 1) {
         return 0;
+    }
 
-    if (index == 2)
+    if (index == 2) {
         return 1;
+    }
 
     return fib_r(index - 1) + fib_r(index - 2);
 } 
@@ -65,8 +71,7 @@ unsigned long long fib_i(int index)
 {
     static unsigned long long memo[1000] = {0};
 
-    if (memo[index] != 0)
-    {
+    if (memo[index] != 0) {
         return memo[index];
     }
     
@@ -79,8 +84,7 @@ unsigned long long fib_r(int index)
 {
     static unsigned long long memo[1000] = {0};
 
-    if (memo[index] != 0)
-    {
+    if (memo[index] != 0) {
         return memo[index];
     }
     
