1f07ba059d9fd3f68dd98ceec600131a13543975
kalmk <111930167+kalmk@users.noreply.github.com>
2024-09-19 08:37:15 -0700

    fixed memo

--- fib.c
+++ fib.c
@@ -2,55 +2,22 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_SIZE 100
-
-unsigned long long fib_i(int times);
-unsigned long long fib_r(int times);
-unsigned long long fib_i_provider(int times);
-unsigned long long fib_r_provider(int times);
-
-unsigned long long memo[MAX_SIZE];
-
-unsigned long long fib_i(int times)
-{
-    if (memo[times] != 0)
-    {
-        return memo[times];
-    }
-    
-    memo[times] = fib_i_provider(times);
-
-    return memo[times];
-}
-
-unsigned long long fib_r(int times)
-{
-    if (memo[times] != 0)
-    {
-        return memo[times];
-    }
-
-    memo[times] = fib_r_provider(times);
-
-    return memo[times];
-}
+unsigned long long fib_i_core(int index);
+unsigned long long fib_r_core(int index);
+unsigned long long fib_i(int index);
+unsigned long long fib_r(int index);
 
 int main(int argc, char *argv[])
 {
-    // First argument is an integer
     int first_arg = atoi(argv[1]);
 
-    // Second argument is "r" or "i"
     char *temp = malloc(sizeof(argv[2]));
     strcpy(temp, argv[2]);
     char second_arg = temp[0];
     free(temp);
     
-    // Add two numbers together
     int N = first_arg;
     
-    // Compute the Nth Fibonacci number according to "i" or "r"
-    // Fibonacci sequence starts with 0 and 1
     if (second_arg == 'i') 
         printf("%llu\n", fib_i(N));
     else if (second_arg == 'r')
@@ -59,21 +26,21 @@ int main(int argc, char *argv[])
     return 0;
 }
 
-unsigned long long fib_i_provider(int times)
+unsigned long long fib_i_core(int index)
 {
-    if (times == 1)
+    if (index == 1)
         return 0;
 
-    if (times == 2)
+    if (index == 2)
         return 1;
 
-    --times;
+    --index;
 
     unsigned long long t1 = 0;
     unsigned long long t2 = 1;
     unsigned long long next_term = t1 + t2;
 
-    for (int i = 3; i <= times; ++i)
+    for (int i = 3; i <= index; ++i)
     {
         t1 = t2;
         t2 = next_term;
@@ -83,13 +50,41 @@ unsigned long long fib_i_provider(int times)
     return next_term;
 }
 
-unsigned long long fib_r_provider(int times)
+unsigned long long fib_r_core(int index)
 {
-    if (times == 1)
+    if (index == 1)
         return 0;
 
-    if (times == 2)
+    if (index == 2)
         return 1;
 
-    return fib_i(times - 1) + fib_i(times - 2);
+    return fib_r(index - 1) + fib_r(index - 2);
 } 
+
+unsigned long long fib_i(int index)
+{
+    static unsigned long long memo[1000] = {0};
+
+    if (memo[index] != 0)
+    {
+        return memo[index];
+    }
+    
+    memo[index] = fib_i_core(index);
+
+    return memo[index];
+}
+
+unsigned long long fib_r(int index)
+{
+    static unsigned long long memo[1000] = {0};
+
+    if (memo[index] != 0)
+    {
+        return memo[index];
+    }
+    
+    memo[index] = fib_r_core(index);
+
+    return memo[index];
+}
